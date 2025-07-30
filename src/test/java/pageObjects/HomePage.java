2861aedde689c296ed79e005a42714d3705683a4
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-07-30 17:53:27 +0530

    30th July 2025

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,29 +1,39 @@
+// This class belongs to the pageObjects package
 package pageObjects;
 
+// Import Selenium's WebDriver to control the browser
 import org.openqa.selenium.WebDriver;
+
+// Import WebElement so we can define clickable elements
 import org.openqa.selenium.WebElement;
+
+// Import FindBy for locating elements using XPath
 import org.openqa.selenium.support.FindBy;
 
+// HomePage class extends BasePage to reuse common WebDriver setup and element initialization
 public class HomePage extends BasePage {
 
+    // Constructor for HomePage
+    // It accepts a WebDriver and passes it to the BasePage using 'super'
     public HomePage(WebDriver driver){
-
-        super(driver);
+        super(driver);  // This initializes the driver and WebElements in the parent class
     }
 
+    // Locate the "My Account" link using its title attribute
     @FindBy(xpath = "//a[@title=\"My Account\"]")
     WebElement lnkMyAccount;
 
-     @FindBy(xpath = "//a[text()='Register']")
+    // Locate the "Register" link using its visible text
+    @FindBy(xpath = "//a[text()='Register']")
     WebElement lnkRegister;
 
-     public void clickMyAccount (){
-         lnkMyAccount.click();
-     }
+    // Method to click on the "My Account" link
+    public void clickMyAccount (){
+        lnkMyAccount.click();  // Simulates user clicking on the "My Account" dropdown
+    }
 
-     public void clickRegister (){
-         lnkRegister.click();
-     }
-
-
+    // Method to click on the "Register" link
+    public void clickRegister (){
+        lnkRegister.click();  // Simulates user clicking on the "Register" option
+    }
 }
